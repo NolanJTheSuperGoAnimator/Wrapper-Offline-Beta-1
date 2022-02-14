55665bb0bc5e60e215ccb766e3689186c1c684de
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-02-14 07:02:01 -0500

    Update main.js

--- wrapper/starter/main.js
+++ wrapper/starter/main.js
@@ -38,9 +38,9 @@ module.exports = {
 			var prefix = mId.substr(0, i);
 			var suffix = mId.substr(i + 1);
 			switch (prefix) {
-				case "m":
-					var moviePath = fUtil.getFileIndex("movie-", ".xml", suffix);
-					var thumbPath = fUtil.getFileIndex("thumb-", ".png", suffix);
+				case "s":
+					var moviePath = fUtil.getFileIndex("starter-", ".xml", suffix);
+					var thumbPath = fUtil.getFileIndex("starter-", ".png", suffix);
 					fs.unlinkSync(moviePath);
 					fs.unlinkSync(thumbPath);
 					caché.clearTable(mId);
@@ -52,69 +52,7 @@ module.exports = {
 			}
 		});
 	},
-	loadZip(mId) {
-		return new Promise((res, rej) => {
-			const i = mId.indexOf("-");
-			const prefix = mId.substr(0, i);
-			const suffix = mId.substr(i + 1);
-			switch (prefix) {
-				case "e": {
-					caché.clearTable(mId);
-					let data = fs.readFileSync(`${exFolder}/${suffix}.zip`);
-					res(data.subarray(data.indexOf(80)));
-					break;
-				}
-				case "m": {
-					let numId = Number.parseInt(suffix);
-					if (isNaN(numId)) res();
-					let filePath = fUtil.getFileIndex("movie-", ".xml", numId);
-					if (!fs.existsSync(filePath)) res();
-
-					const buffer = fs.readFileSync(filePath);
-					if (!buffer || buffer.length == 0) res();
-
-					try {
-						parse.packMovie(buffer, mId).then((pack) => {
-						parse.packXml(buffer, mId).then(v => res(v));
-							caché.saveTable(mId, pack.caché);
-							res(pack.zipBuf);
-						});
-						break;
-					} catch (e) {
-						res();
-					}
-				}
-				default:
-					res();
-			}
-		});
-	},
-	loadXml(movieId) {
-		return new Promise(async (res, rej) => {
-			const i = movieId.indexOf("-");
-			const prefix = movieId.substr(0, i);
-			const suffix = movieId.substr(i + 1);
-			switch (prefix) {
-				case "m": {
-					const fn = fUtil.getFileIndex("movie-", ".xml", suffix);
-					if (fs.existsSync(fn)) res(fs.readFileSync(fn));
-					else rej();
-					break;
-				}
-				case "e": {
-					const fn = `${exFolder}/${suffix}.zip`;
-					if (!fs.existsSync(fn)) return rej();
-					parse
-						.unpackMovie(nodezip.unzip(fn))
-						.then((v) => res(v))
-						.catch((e) => rej(e));
-					break;
-				}
-				default:
-					rej();
-			}
-		});
-	},
+	// The cores to load starter is in the main.js file in the movie folder. thanks for reading. -Joseph Animate 2021
 	thumb(movieId) {
 		return new Promise(async (res, rej) => {
 			if (!movieId.startsWith("s-")) return;
@@ -131,41 +69,5 @@ module.exports = {
 			table.unshift({ id: id });
 		}
 		return table;
-	},
-	meta(movieId) {
-		return new Promise(async (res, rej) => {
-			if (!movieId.startsWith("m-")) return;
-			const n = Number.parseInt(movieId.substr(2));
-			const fn = fUtil.getFileIndex("movie-", ".xml", n);
-
-			const fd = fs.openSync(fn, "r");
-			const buffer = Buffer.alloc(256);
-			fs.readSync(fd, buffer, 0, 256, 0);
-			const begTitle = buffer.indexOf("<title>") + 16;
-			const endTitle = buffer.indexOf("]]></title>");
-			const title = buffer.slice(begTitle, endTitle).toString().trim();
-
-			const begDesc = buffer.indexOf("<desc>") + 15;
-			const endDesc = buffer.indexOf("]]></desc>");
-			const longDesc = buffer.slice(begDesc, endDesc).toString().trim();
-			const desc = truncate(longDesc, 51);
-
-			const begDuration = buffer.indexOf('duration="') + 10;
-			const endDuration = buffer.indexOf('"', begDuration);
-			const duration = Number.parseFloat(buffer.slice(begDuration, endDuration));
-			const min = ("" + ~~(duration / 60)).padStart(2, "0");
-			const sec = ("" + ~~(duration % 60)).padStart(2, "0");
-			const durationStr = `${min}:${sec}`;
-
-			fs.closeSync(fd);
-			res({
-				date: fs.statSync(fn).mtime,
-				durationString: durationStr,
-				duration: duration,
-				title: title,
-				desc: desc,
-				id: movieId,
-			});
-		});
 	},
 };
